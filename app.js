e204a74f5b9d90146bd1c2f5e72cc98d2e1b37e0
burnoutberni <bernhard.hayden@gmail.com>
2015-09-07 11:23:50 +0200

    fixed minor bug in sharing url

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ createCookie("padroot", settings.padRoot, 30);
 
 $("#padServer").val(settings.padServer);
 $("#padRoot").val(settings.padRoot);
-$("#share").val(window.location+"?padserver="+settings.padServer+"&padroot="+settings.padRoot);
+$("#share").val(location.protocol + '//' + location.host + location.pathname +"?padserver="+settings.padServer+"&padroot="+settings.padRoot);
 
 var questionIndex = 0;
 var questionCountClosed = 0;
@@ -117,14 +117,14 @@ $("#selectSettings").click(function() {
 
 $("#padServer").blur(function() {
 	settings.padServer = $("#padServer").val();
-	$("#share").val(window.location+"?padserver="+settings.padServer+"&padroot="+settings.padRoot);
+	$("#share").val(location.protocol + '//' + location.host + location.pathname +"?padserver="+settings.padServer+"&padroot="+settings.padRoot);
 	createCookie("padserver", settings.padServer, 30);
 	resetLoaded(".question");
 });
 
 $("#padRoot").blur(function() {
 	settings.padRoot = $("#padRoot").val();
-	$("#share").val(window.location+"?padserver="+settings.padServer+"&padroot="+settings.padRoot);
+	$("#share").val(location.protocol + '//' + location.host + location.pathname +"?padserver="+settings.padServer+"&padroot="+settings.padRoot);
 	createCookie("padroot", settings.padRoot, 30);
 	resetLoaded(".question");
 });
